795f5ae40a4d1f9bf33c8e427dccb6e23840b9ba
Jackson Dolman <mail@dolmios.com>
2022-12-20 12:16:26 -0500

    chore: bump deps

--- configs/swc/base.js
+++ configs/swc/base.js
@@ -1,24 +1,18 @@
 // T-3235 - don't use this swc config for now (if swcrc can even be extended?)
-module.exports =
-  {
-    $schema:
-      "https://json.schemastore.org/swcrc",
-    jsc: {
-      parser:
-        {
-          syntax:
-            "typescript",
-          jsx: true,
-          tsx: true,
-          dynamicImport: true,
-          decorators: true,
-        },
-      target:
-        "esnext",
+module.exports = {
+  $schema: "https://json.schemastore.org/swcrc",
+  jsc: {
+    parser: {
+      syntax: "typescript",
+      jsx: true,
+      tsx: true,
+      dynamicImport: true,
+      decorators: true,
     },
-    minify: false,
-    module:
-      {
-        type: "es6",
-      },
-  };
+    target: "esnext",
+  },
+  minify: false,
+  module: {
+    type: "es6",
+  },
+};
